e0f856847696973b2b3771c3baaeb7fc57076fff
Dario Olivares <fcdarios@gmail.com>
2020-03-22 11:43:30 -0600

    Actualizacion

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ Route::get('/', "BusinessController@home") ->name("home");
 Route::get('/about', "BusinessController@about") ->name('about');
 Route::get('/contact', "BusinessController@contact") ->name('contact');
 Route::get('/blog', "BusinessController@blog") ->name('blog');
-
 Route::get('/admin/blog', 'BlogController@index')->name('admin.blog');
 Route::get('/admin/create', 'BlogController@create')->name('admin.create');
 Route::post('/admin/store', 'BlogController@store') -> name('admin.store');
